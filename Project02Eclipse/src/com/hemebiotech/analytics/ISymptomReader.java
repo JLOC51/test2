e143891c4fe6b893344d2ab3cb1832dcc27c42ba
JLOC51 <nguyen.jluc@gmail.com>
2020-01-27 22:08:34 +0100

    1er feature

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,6 +1,8 @@
-package com.hemebiotech.analytics;
+﻿package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Anything that will read symptom data from a source
@@ -13,8 +15,15 @@ import java.util.List;
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
+	 * @param string 
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	
+	
+	public void ReadSymptomDataFromFile ();
+	
+	public List<String> GetSymptoms () throws IOException;
+
+	
 }
